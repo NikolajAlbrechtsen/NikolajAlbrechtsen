3d0089fcb0e4f8fd4200162308849633838a606f
ProspectDesigns <nikolaj2503@gmail.com>
2019-05-01 19:59:20 +0200

    javscript

---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+  const sandwich = document.querySelector('.sandwich');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li')
+
+  sandwich.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+  });
+  navLinks.forEach((link, index) => {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}`;
+  });
+}
+
+navSlide();
